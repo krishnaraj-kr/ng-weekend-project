1d4ff8e84640f7489b1d6102b093b3d36f3fc434
Krishnaraj <krishnaraj.icafs@gmail.com>
2019-01-03 22:20:44 +0530

    Milestone - 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngV7';
-}
+export class AppComponent { }
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+
+//import { Observable } from 'rxjs';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { of } from 'rxjs/observable/of';
+
+import { catchError, tap } from 'rxjs/operators';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+
+import { IProduct } from './product';
+
+@Injectable()
+export class ProductService {
+    private productsUrl = 'api/products';
+
+    constructor(private http: HttpClient) { }
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productsUrl);;
+    }
+
+    getProduct(id: number): Observable<IProduct> {
+        if (id === 0) {
+            return of(this.initializeProduct());
+        }
+        const url = `${this.productsUrl}/${id}`;
+        return this.http.get<IProduct>(url);
+    }
+
+    saveProduct(product: IProduct): Observable<IProduct> {
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        if (product.id === 0) {
+            return this.createProduct(product, headers);
+        }
+        return this.updateProduct(product, headers);
+    }
+
+    deleteProduct(id: number): Observable<IProduct> {
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+        const url = `${this.productsUrl}/${id}`;
+        return this.http.delete<IProduct>(url, { headers: headers} );
+    }
+
+    private createProduct(product: IProduct, headers: HttpHeaders): Observable<IProduct> {
+        product.id = null;
+        return this.http.post<IProduct>(this.productsUrl, product,  { headers: headers} );
+    }
+
+    private updateProduct(product: IProduct, headers: HttpHeaders): Observable<IProduct> {
+        const url = `${this.productsUrl}/${product.id}`;
+        return this.http.put<IProduct>(url, product, { headers: headers} );
+    }
+
+    private initializeProduct(): IProduct {
+        // Return an initialized object
+        return {
+            'id': 0,
+            productName: '',
+            productCode: '',
+            category: '',
+            tags: [],
+            releaseDate: '',
+            price: 0,
+            description: '',
+            starRating: 0,
+            imageUrl: ''
+        };
+    }
+
+}
